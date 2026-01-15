80beffd9e3056042fe2104202156fd9c744260d2
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2026-01-15 17:43:21 +0700

    fix: serialize_many_error

--- src/errors/serialize_many_error.rs
+++ src/errors/serialize_many_error.rs
@@ -1,5 +1,4 @@
 use crate::errors::unsupported_format_error::UnsupportedFormatError;
-use crate::format::Format;
 use derive_more::{Display, Error, From};
 
 #[derive(Error, Display, From, Debug)]
@@ -12,10 +11,4 @@ pub enum SerializeManyError {
     Csv(csv::Error),
     #[cfg(feature = "csv")]
     FromUtf8(std::string::FromUtf8Error),
-}
-
-impl From<Format> for SerializeManyError {
-    fn from(value: Format) -> Self {
-        Self::UnsupportedFormat(value.into())
-    }
 }
