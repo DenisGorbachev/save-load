3884cebb09cc86060c4012214bff773771484149
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2026-01-21 05:19:53 +0700

    fix: deserialize_many_error

--- src/errors/deserialize_many_error.rs
+++ src/errors/deserialize_many_error.rs
@@ -1,6 +1,5 @@
 use crate::errors::deserialize_one_error::DeserializeOneError;
 use crate::errors::unsupported_format_error::UnsupportedFormatError;
-use crate::format::Format;
 use derive_more::{Display, Error, From};
 
 #[derive(Error, Display, From, Debug)]
@@ -9,10 +8,4 @@ pub enum DeserializeManyError {
     UnsupportedFormat(UnsupportedFormatError),
     DeserializeOneError(DeserializeOneError),
     IoError(std::io::Error),
-}
-
-impl From<Format> for DeserializeManyError {
-    fn from(value: Format) -> Self {
-        Self::UnsupportedFormat(value.into())
-    }
 }
